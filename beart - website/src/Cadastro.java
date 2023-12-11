d0ea1334be81877f810b1c91226450ccba478319
kagayemo <levi.tobio2505@gmail.com>
2023-12-11 15:53:14 -0300

    att

--- beart - website/src/Cadastro.java
+++ beart - website/src/Cadastro.java
@@ -9,25 +9,6 @@ public class Cadastro {
     public void adicionarEscola(Escola e) {
         listaDeEscolas.add(e);
 
-        // Metódo para adicionar as escolas na lista
-    }
-
-    public Escola logarConta(String loginEscolaCodigo, String loginEscolaSenha) {
-        String verificacao = "Usuário ou senha incorretos! Tente novamente";
-        for (Escola e : this.listaDeEscolas) {
-            if (e.codigoInstituicao.equals(loginEscolaCodigo) && e.senha.equals(loginEscolaSenha)) {
-
-                System.out.println("Login realizado com sucesso!");
-                return e;
-                // Se a informação do usuário bater com o que foi cadastrado, retorna o menu
-                // para a instituição.
-
-            } else {
-                System.out.println(verificacao);
-                // Se a informação não bater, retorna a verificação com a mensagem
-            }
-        }
-        return null;
     }
 
 }
--- beart - website/src/Principal.java
+++ beart - website/src/Principal.java
@@ -30,41 +30,47 @@ public class Principal {
 
                     loginEscolaCodigo = EntradaSaida.entrarEscola("o código");
                     loginEscolaSenha = EntradaSaida.entrarEscola("a senha");
-                    e = c.logarConta(loginEscolaCodigo, loginEscolaSenha);
-
                     // Login da instituição
-                        do {
-                            opcaoEscola = EntradaSaida.escolherOpcaoEscola();
-                            switch (opcaoEscola) {
-                                case 1:
-                                    Aluno a = new Aluno();// < ISSO TÁ CERTO!!!!!!!!!!!!!!!!
 
-                                    a.nome = EntradaSaida.cadastrarAluno("o nome");
-                                    a.matricula = EntradaSaida.cadastrarAluno("a matrícula");
-                                    a.senha = EntradaSaida.cadastrarAluno("a senha");
-                                    e.adicionarAluno(a);
-                                    break;
+                    for (Escola escola : c.listaDeEscolas) {
+                        if (escola.codigoInstituicao.equals(loginEscolaCodigo)
+                                && escola.senha.equals(loginEscolaSenha)) {
+                            System.out.println("Login realizado com sucesso!");
 
-                                case 2:
+                            do {
+                                opcaoEscola = EntradaSaida.escolherOpcaoEscola();
+                                switch (opcaoEscola) {
+                                    case 1:
+                                        Aluno a = new Aluno();// < ISSO TÁ CERTO!!!!!!!!!!!!!!!!
 
-                                    // Remover um aluno
-                                    break;
-                                case 3:
+                                        a.nome = EntradaSaida.cadastrarAluno("o nome");
+                                        a.matricula = EntradaSaida.cadastrarAluno("a matrícula");
+                                        a.senha = EntradaSaida.cadastrarAluno("a senha");
+                                        escola.adicionarAluno(a);
+                                        break;
 
-                                    if (!e.listaDeAlunos.isEmpty()) {
-                                        EntradaSaida.mostrarAlunos(e.listarAlunos());
-                                        // funciona maisomeno :)
-                                    } else {
+                                    case 2:
 
-                                    }
-                                    break;
-                                case 4:
-                                    // Sair da conta
-                                    EntradaSaida.escolherOpcao();
-                                    break;
-                            }
-                        } while (opcaoEscola != 4);
-                        
+                                        // Remover um aluno
+                                        break;
+                                    case 3:
+
+                                        if (!escola.listaDeAlunos.isEmpty()) {
+                                            EntradaSaida.mostrarAlunos(escola.listarAlunos());
+                                            // funciona maisomeno :)
+                                        } else {
+
+                                        }
+                                        break;
+                                    case 4:
+                                        // Sair da conta
+                                        break;
+                                }
+                            } while (opcaoEscola != 4);
+                        } else {
+                            System.out.println("Usuário ou senha incorretos! Tente novamente");
+                        }
+                    }
 
                     break;
 
@@ -81,9 +87,10 @@ public class Principal {
             }
 
         } while (opcao != 6);
+
     }
 }
 
 // everybody gon' respect de shooter, but the one in the front of the gun lives
 // forever.
-// why god why god do i got to suffe
+// why god why god do i got to suffer
