9b675de8258dc19e546231d68a54aa2ef0eecb69
Levi <143547667+kagayemo@users.noreply.github.com>
2024-01-11 21:56:24 -0300

    .

--- beart - website/src/Cursos.java
+++
@@ -1,9 +0,0 @@
-public class Cursos {
-
-    public String nomeCurso;
-    public String descricao;
-    public String enderecoCurso;
-    public String professor;
-    public String dataHora;
-    
-}
